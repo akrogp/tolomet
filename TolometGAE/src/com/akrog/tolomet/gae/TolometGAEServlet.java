8e7db69c7df3f4ad77caf31b850bd8a14c116d61
gorka.prieto <gorka.prieto@gmail.com>
2014-10-19 18:52:48 +0000

    Included TolometGAE in the repository

---
+++ TolometGAE/src/com/akrog/tolomet/gae/TolometGAEServlet.java
@@ -0,0 +1,16 @@
+package com.akrog.tolomet.gae;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class TolometGAEServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("No web interface available (at the momment)");
+	}
+}
