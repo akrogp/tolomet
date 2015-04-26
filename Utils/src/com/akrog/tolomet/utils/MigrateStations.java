dcc664d525c23fd19962841b03666c9971fb8ca1
gorka.prieto <gorka.prieto@gmail.com>
2015-04-26 20:36:34 +0000

    Started METAR stations integration

--- Utils/src/com/akrog/tolomet/utils/MigrateStations.java
+++ Utils/src/com/akrog/tolomet/utils/MigrateStations.java
@@ -1,17 +1,31 @@
 package com.akrog.tolomet.utils;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.akrog.tolomet.Manager;
 import com.akrog.tolomet.Station;
 
 public class MigrateStations {
 	public static void main( String[] args ) throws IOException {
-		Manager tolomet = new Manager();
+		Manager tolomet = new Manager();		
+		List<Station> stations = new ArrayList<Station>(tolomet.getAllStations());
+		stations.addAll(loadMetars());
+		Collections.sort(stations, new Comparator<Station>() {
+			@Override
+			public int compare(Station o1, Station o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
 		DataOutputStream dos = new DataOutputStream(new FileOutputStream("/home/gorka/stations.dat"));
-		for( Station station : tolomet.getAllStations() ) {
+		for( Station station : stations ) {
 			System.out.println(String.format("%s ... ", station.toString()));
 			saveStation(dos,station);
 		}
@@ -19,9 +33,12 @@ public class MigrateStations {
 		dos.close();
 	}
 	
-	/*public static void main( String[] args ) throws Exception {
-		stations.txt
-	}*/
+	private static List<Station> loadMetars() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("/home/gorka/MyProjects/Android/Tolomet/Docs/metar.txt"));
+		List<Station> list = new ArrayList<Station>();
+		br.close();
+		return list;
+	}
 		
 	private static void saveStation( DataOutputStream os, Station station ) throws IOException {
 		os.writeUTF(station.getCode());
