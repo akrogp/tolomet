8b8fa1e7f117551fc29db5eab48992f020d54ddd
gorka.prieto@gmail.com <gorka.prieto@gmail.com@5cc22062-1003-9af5-2f0f-73ebc811f213>
2015-05-21 20:42:03 +0000

    - Included util to manually add stations - Included Kanpezu station

--- trunk/Utils/src/com/akrog/tolomet/utils/FixStations.java
+++ trunk/Utils/src/com/akrog/tolomet/utils/FixStations.java
@@ -1,10 +1,12 @@
 package com.akrog.tolomet.utils;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,13 +16,18 @@ import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import uk.me.jstott.jcoord.LatLng;
+import uk.me.jstott.jcoord.UTMRef;
+
 import com.akrog.tolomet.Station;
 import com.akrog.tolomet.providers.WindProviderType;
 
 public class FixStations {
 	public static void main(String[] args) throws IOException {
 		logger.info("Started");
-		List<Station> stations = loadStations();
+		String inPath = "/home/gorka/MyProjects/Android/Tolomet/TolometCore/src/res/stations_ES.dat";
+		//String inPath = "/home/gorka/MyProjects/Android/Tolomet/Docs/stations_world.dat";
+		List<Station> stations = loadStations(inPath);
 		addNewStations(stations);
 		sortStations(stations);
 		for( Station station : stations ) {
@@ -31,7 +38,74 @@ public class FixStations {
 	}		
 
 	private static void addNewStations(List<Station> stations) {
-		//stations.addAll(PradesStations.getStations());
+		try {
+			//stations.addAll(MetarStations.loadMetars());
+			//stations.addAll(PradesStations.getStations());
+			Station station;
+			while( (station=askNew()) != null )
+				stations.add(station);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+
+	private static Station askNew() throws IOException {
+		if( !askParameter("¿Introducir nueva estación? (s/n)").equalsIgnoreCase("s") )			
+			return null;
+		Station station = new Station();
+		station.setCode(askParameter("Código"));
+		station.setName(askParameter("Nombre"));
+		station.setProviderType(WindProviderType.valueOf(askParameter("Proveedor", (Object[])WindProviderType.values())));
+		station.setCountry(askParameter("Código país").toUpperCase());
+		station.setRegion(Integer.parseInt(askParameter("Región")));
+		if( askParameter("Formato coordenadas (ll/utm)").equalsIgnoreCase("ll") ) {
+			station.setLatitude(Double.parseDouble(askParameter("Latitud")));
+			station.setLongitude(Double.parseDouble(askParameter("Longitud")));
+		} else {
+			double x = Double.parseDouble(askParameter("Easting"));
+			double y = Double.parseDouble(askParameter("Northing"));
+			System.out.println("http://whatutmzoneamiin.blogspot.com.es/p/map.html");
+			UTMRef utm = new UTMRef(
+				x, y,
+				Character.toUpperCase(askParameter("latZone").charAt(0)),
+				Integer.parseInt(askParameter("lngZone")));
+			LatLng ll = utm.toLatLng();
+			station.setLatitude(ll.getLat());
+			station.setLongitude(ll.getLng());
+		}
+		showStation(station);
+		return askParameter("¿OK? (s/n)").equalsIgnoreCase("s") ? station : null;
+	}
+	
+	private static void showStation(Station station) {
+		System.out.println(station.getCode());
+		System.out.println(station.getName());
+		System.out.println(station.getProviderType());
+		System.out.println(station.getCountry());
+		System.out.println(station.getRegion());
+		System.out.println(station.getLatitude());
+		System.out.println(station.getLongitude());		
+	}
+
+	private static String askParameter( String msg, Object... opts ) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		if( opts.length == 0 ) {
+			System.out.print(String.format("%s: ", msg));
+			return br.readLine();
+		}
+		System.out.println(String.format("%s:", msg));
+		int i = 0;
+		for( Object opt : opts )
+			System.out.println(String.format("%d) %s", i++, opt));
+		do {
+			try {
+				System.out.print("Elección -> ");
+				i = Integer.parseInt(br.readLine());
+			} catch(NumberFormatException e ) {
+				i = -1;
+			}
+		} while(i < 0 || i >= opts.length);
+		return opts[i].toString();
 	}
 
 	private static void sortStations(List<Station> stations) {
@@ -63,14 +137,13 @@ public class FixStations {
 		station.setName(newName);
 	}
 	
-	private static List<Station> loadStations() {
+	private static List<Station> loadStations(String path) {
 		List<Station> stations = new ArrayList<Station>();
 		Set<String> names = new HashSet<String>();
 		Station station;
 		DataInputStream dis = null;
 		try {
-			//dis = new DataInputStream(new FileInputStream("/home/gorka/MyProjects/Android/Tolomet/TolometCore/src/res/stations_ES.dat"));
-			dis = new DataInputStream(new FileInputStream("/home/gorka/MyProjects/Android/Tolomet/Docs/stations_world.dat"));		
+			dis = new DataInputStream(new FileInputStream(path));		
 			while (true) {
 				station = new Station();
 				station.setCode(dis.readUTF());
