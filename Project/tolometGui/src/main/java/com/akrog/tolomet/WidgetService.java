1e4ee25f73e1114c1f2d1bbf28d53a1c078e9cbe
Gorka Prieto <gorka.prieto@gmail.com>
2016-05-12 20:18:44 +0200

    First widget test working

--- Project/tolometGui/src/main/java/com/akrog/tolomet/WidgetService.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/WidgetService.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.RemoteViews;
@@ -14,31 +15,54 @@ import com.akrog.tolomet.providers.WindProviderType;
  * Created by gorka on 11/05/16.
  */
 public class WidgetService extends Service {
-    @Override
-    public void onStart(Intent intent, int startId) {
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
-        int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-        for (int widgetId : allWidgetIds) {
-            // Display data
-            Station station = new Station();
-            station.setCode("C042");
-            station.setProviderType(WindProviderType.Euskalmet);
-            model.setCurrentStation(station);
-            model.refresh();
-            String txt = model.getSummary(false);
-            RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
-            remoteViews.setTextViewText(R.id.widget_summary, txt);
 
-            // Register an onClickListener
-            Intent clickIntent = new Intent(this.getApplicationContext(), WidgetProvider.class);
-            clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.widget_summary, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
-        }
-        stopSelf();
-        super.onStart(intent, startId);
+    private static class WidgetData {
+        String[] allWidgetSummaries;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+        final int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+        new AsyncTask<Void, Void, WidgetData>() {
+            @Override
+            protected WidgetData doInBackground(Void... params) {
+                Station station = new Station();
+                station.setCode("C042");
+                station.setProviderType(WindProviderType.Euskalmet);
+                model.setCurrentStation(station);
+                model.refresh();
+                String txt = model.getSummary(false);
+                WidgetData widgetData = new WidgetData();
+                widgetData.allWidgetSummaries = new String[allWidgetIds.length];
+                for( int i = 0; i < allWidgetIds.length; i++ )
+                    widgetData.allWidgetSummaries[i] = txt;
+                return widgetData;
+            }
+
+            @Override
+            protected void onPostExecute(WidgetData widgetData) {
+                int i = 0;
+                for (int widgetId : allWidgetIds) {
+                    RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_layout);
+                    remoteViews.setTextViewText(R.id.widget_summary, widgetData.allWidgetSummaries[i++]);
+                    Intent clickIntent = new Intent(getApplicationContext(), WidgetProvider.class);
+                    clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                    clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    remoteViews.setOnClickPendingIntent(R.id.widget_summary, pendingIntent);
+                    appWidgetManager.updateAppWidget(widgetId, remoteViews);
+                }
+                stopSelf();
+            }
+
+            @Override
+            protected void onCancelled() {
+                stopSelf();
+                super.onCancelled();
+            }
+        }.execute();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
