b76d8ebb92280ccc4971e343d576f91d3a6c4b9a
Gorka Prieto <gorka.prieto@gmail.com>
2016-05-16 12:45:22 +0200

    Included alarm to update widgets

--- Project/tolometGui/src/main/java/com/akrog/tolomet/WidgetProvider.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/WidgetProvider.java
@@ -1,5 +1,7 @@
 package com.akrog.tolomet;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
@@ -10,20 +12,49 @@ import android.content.Intent;
  * Created by gorka on 11/05/16.
  */
 public class WidgetProvider extends AppWidgetProvider {
+    public static String FORCE_WIDGET_UPDATE = "com.akrog.tolomet.FORCE_WIDGET_UPDATE";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarm.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME,
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                getUpdateIntent(context));
+    }
+
+    private PendingIntent getUpdateIntent(Context context) {
+        return PendingIntent.getBroadcast(context,0,new Intent(FORCE_WIDGET_UPDATE),PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(getUpdateIntent(context));
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        //super.onUpdate(context, appWidgetManager, appWidgetIds);
-        ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-
-        // Build the intent to call the service
-        Intent intent = new Intent(context.getApplicationContext(), WidgetService.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-
-        // Update the widgets via the service
-        context.startService(intent);
+        startService(context, appWidgetIds);
     }
 
-    private static final String ACTION_CLICK = "ACTION_CLICK";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if( FORCE_WIDGET_UPDATE.equals(intent.getAction()) ) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+            startService(context,appWidgetIds);
+        }
+    }
+
+    private void startService(Context context, int[] appWidgetIds) {
+        Intent intent = new Intent(context.getApplicationContext(), WidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        context.startService(intent);
+    }
 }
