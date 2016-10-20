09ecb2bbf07195c812b7218d9c66f006b62dcf47
Gorka Prieto <gorka.prieto@gmail.com>
2016-10-20 13:40:07 +0200

    Replaced ProgressDialog with a ProgressBar inside the toolbar

--- Project/tolometGui/src/main/java/com/akrog/tolomet/presenters/Downloader.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/presenters/Downloader.java
@@ -1,9 +1,8 @@
 package com.akrog.tolomet.presenters;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.akrog.tolomet.ChartsActivity;
@@ -12,33 +11,25 @@ import com.akrog.tolomet.R;
 
 public class Downloader extends AsyncTask<Void, Void, Void> {
 	private ChartsActivity tolomet;
-	private ProgressDialog progress;
+	private ProgressBar progressBar;
 	private final Model model = Model.getInstance();
 
 	public Downloader(ChartsActivity tolomet) {
 		this.tolomet = tolomet;
-		progress = new ProgressDialog(this.tolomet);
-		progress.setMessage( this.tolomet.getString(R.string.Downloading)+"..." );
-        progress.setTitle( "" );//getString(R.string.Progress) );
-        progress.setIndeterminate(true);
-        progress.setCancelable(true);
-        progress.setOnCancelListener(new OnCancelListener(){
-        	public void onCancel(DialogInterface dialog) {
-        		cancel(true);
-        	}
-        });
+		progressBar = tolomet.getProgressBar();
 	}
 	
 	@Override
     protected void onPreExecute() {
-        super.onPreExecute();	        
-        progress.show();
+        super.onPreExecute();
+		progressBar.setIndeterminate(true);
+		progressBar.setVisibility(View.VISIBLE);
     }
 	
 	@Override
 	protected void onCancelled() {
 		super.onCancelled();
-		//this.progress.dismiss();
+		progressBar.setVisibility(View.GONE);
 		Toast.makeText(tolomet,tolomet.getString(R.string.DownloadCancelled),Toast.LENGTH_SHORT).show();
 		tolomet.onCancelled();
 	}	
@@ -53,7 +44,7 @@ public class Downloader extends AsyncTask<Void, Void, Void> {
     protected void onPostExecute(Void result) {
         super.onPostExecute(result);
         try {
-        	progress.dismiss();
+        	progressBar.setVisibility(View.GONE);
         } catch( Exception e ) {}
         tolomet.onDownloaded();
     }
