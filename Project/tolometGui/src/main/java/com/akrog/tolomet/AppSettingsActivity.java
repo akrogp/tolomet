629879ec6c562be075d2b623688790244944c529
Gorka Prieto <gorka.prieto@gmail.com>
2016-11-25 16:12:20 +0100

    Update language also in settings activity

--- Project/tolometGui/src/main/java/com/akrog/tolomet/AppSettingsActivity.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/AppSettingsActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.gunhansancar.android.sdk.helper.LocaleHelper;
+
 public class AppSettingsActivity extends SettingsActivity {
     private final Intent resultIntent = new Intent();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        LocaleHelper.onCreate(this);
 		onCreate(savedInstanceState,R.xml.preferences);
+        if( getIntent() != null )
+            resultIntent.putExtras(getIntent());
 	}
 
     @Override
@@ -23,5 +28,9 @@ public class AppSettingsActivity extends SettingsActivity {
     public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
         super.onSharedPreferenceChanged(sp, key);
         resultIntent.putExtra(key,true);
+        if( key.equals(LocaleHelper.SELECTED_LANGUAGE) ) {
+            getIntent().putExtras(resultIntent);
+            recreate();
+        }
     }
 }
