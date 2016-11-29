64df71dc78001e0dc8478be0c1f8120a9404dbda
Gorka Prieto <gorka.prieto@gmail.com>
2016-11-29 16:48:41 +0100

    Implemented MeteoClimatic provider

--- Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoClimaticProvider.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoClimaticProvider.java
@@ -1,7 +1,11 @@
 package com.akrog.tolomet.providers;
 
+import com.akrog.tolomet.Meteo;
 import com.akrog.tolomet.Station;
 import com.akrog.tolomet.io.Downloader;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by gorka on 28/11/16.
@@ -14,6 +18,7 @@ public class MeteoClimaticProvider extends BaseProvider {
 
     @Override
     public void configureDownload(Downloader downloader, Station station) {
+        downloader.setUrl(String.format("http://www.meteoclimatic.net/feed/rss/%s",station.getCode()));
     }
 
     @Override
@@ -22,17 +27,45 @@ public class MeteoClimaticProvider extends BaseProvider {
     }
 
     @Override
-    public void updateStation(Station station, String data) {
+    public void updateStation(Station station, String data) throws Exception {
+        int begin = data.indexOf("Actualizado:");
+        if( begin < 0 ) return;
+        int end = data.indexOf("</li>",begin);
+        if( end < 0 ) return;
+        long stamp = df.parse(data.substring(begin+12,end)).getTime();
 
+        begin = data.indexOf(String.format("[[<%s;", station.getCode()));
+        if( begin < 0 ) return;
+        end = data.indexOf("]]",begin);
+        if( end < 0 ) return;
+        String[] fields = data.substring(begin,end).replaceAll(",",".").split("\\)?;\\(");
+        String[] subfields;
+        Meteo meteo = station.getMeteo();
+
+        subfields = fields[1].split(";");
+        meteo.getAirTemperature().put(stamp,Double.parseDouble(subfields[0]));
+
+        subfields = fields[2].split(";");
+        meteo.getAirHumidity().put(stamp,Double.parseDouble(subfields[0]));
+
+        subfields = fields[3].split(";");
+        meteo.getAirPressure().put(stamp,Double.parseDouble(subfields[0]));
+
+        subfields = fields[4].split(";");
+        meteo.getWindSpeedMed().put(stamp,Double.parseDouble(subfields[0]));
+        meteo.getWindSpeedMax().put(stamp,Double.parseDouble(subfields[1]));
+        meteo.getWindDirection().put(stamp,Double.parseDouble(subfields[2]));
     }
 
     @Override
     public String getInfoUrl(String code) {
-        return null;
+        return String.format("http://www.meteoclimatic.net/perfil/%s#toggle",code);
     }
 
     @Override
     public String getUserUrl(String code) {
-        return null;
+        return String.format("http://www.meteoclimatic.net/perfil/%s#content",code);
     }
+
+    private static final DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm z");
 }
