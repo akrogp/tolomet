e95e0cec659465a156f0f015b75e7d2d398cb9a1
Gorka Prieto <gorka.prieto@gmail.com>
2018-10-05 12:56:41 +0200

    Changed widget service as foreground to avoid problems with Android 8

--- Project/tolometGui/src/main/java/com/akrog/tolomet/ui/widget/WidgetService.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/ui/widget/WidgetService.java
@@ -1,16 +1,30 @@
 package com.akrog.tolomet.ui.widget;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.os.AsyncTaskCompat;
+
+import com.akrog.tolomet.R;
 
 /**
  * Created by gorka on 11/05/16.
  */
 public class WidgetService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+        Notification notification = createNotification();
+        startForeground(1, notification);
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, final int startId) {
@@ -49,5 +63,27 @@ public class WidgetService extends Service {
         return null;
     }
 
+    private Notification createNotification() {
+        return new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_widget))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            return;
+        CharSequence name = getString(R.string.widget_channel_name);
+        String description = getString(R.string.notification_widget);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
     private AsyncTask<Void, Void, Void> task;
+    private static final String CHANNEL_ID = "com.akrog.tolomet.channel.widget";
 }
