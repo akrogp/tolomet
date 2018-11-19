db77ddc120880911ee2c7272cce9021f2742d052
Gorka Prieto <gorka.prieto@gmail.com>
2018-11-19 18:36:17 +0100

    Saved checked state

--- Project/tolometGui/src/main/java/com/akrog/tolomet/viewmodel/ProviderWrapper.java
+++ Project/tolometGui/src/main/java/com/akrog/tolomet/viewmodel/ProviderWrapper.java
@@ -5,6 +5,9 @@ import com.akrog.tolomet.providers.WindProviderType;
 public class ProviderWrapper {
     private final WindProviderType type;
     private int iconId;
+    private int stations;
+    private String date;
+    private boolean checked;
 
     public ProviderWrapper(WindProviderType type) {
         this.type = type;
@@ -20,5 +23,29 @@ public class ProviderWrapper {
 
     public int getIconId() {
         return iconId;
+    }
+
+    public void setStations(int stations) {
+        this.stations = stations;
+    }
+
+    public int getStations() {
+        return stations;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public boolean isChecked() {
+        return checked;
     }
 }
