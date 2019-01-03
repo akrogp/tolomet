c0cd10916af0e0e8d46952755e171fc90d514c0b
Gorka Prieto <gorka.prieto@gmail.com>
2019-01-03 19:24:40 +0100

    bugfix: locales in UTM string

--- Project/tolometCore/src/main/java/com/akrog/tolomet/Utils.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/Utils.java
@@ -2,9 +2,11 @@ package com.akrog.tolomet;
 
 import com.ibm.util.CoordinateConversion;
 
+import java.util.Locale;
+
 public class Utils {
     public static void utm2ll(Station station, int zone, char band) {
-        String utm = String.format("%d %c %f %f", zone, Character.toUpperCase(band), station.getLongitude(), station.getLatitude());
+        String utm = String.format(Locale.US, "%d %c %f %f", zone, Character.toUpperCase(band), station.getLongitude(), station.getLatitude());
         CoordinateConversion conv = new CoordinateConversion();
         double[] ll = conv.utm2LatLon(utm);
         station.setLatitude(ll[0]);
