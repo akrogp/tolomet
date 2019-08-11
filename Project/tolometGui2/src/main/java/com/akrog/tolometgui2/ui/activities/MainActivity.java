61bd3b682fac54132437909779277929f24833a0
Gorka Prieto <gorka.prieto@gmail.com>
2019-08-11 15:50:18 +0200

    Created navigation skeleton

--- Project/tolometGui2/src/main/java/com/akrog/tolometgui2/ui/activities/MainActivity.java
+++ Project/tolometGui2/src/main/java/com/akrog/tolometgui2/ui/activities/MainActivity.java
@@ -76,17 +76,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_charts) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_help) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_report) {
 
         }
 
