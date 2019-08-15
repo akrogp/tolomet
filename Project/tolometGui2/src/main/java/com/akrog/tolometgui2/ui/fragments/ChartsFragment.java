82ae1864e88cf64fc1179a68df868c7f3af4de4c
Gorka Prieto <gorka.prieto@gmail.com>
2019-08-15 21:28:49 +0200

    Bugfix

--- Project/tolometGui2/src/main/java/com/akrog/tolometgui2/ui/fragments/ChartsFragment.java
+++ Project/tolometGui2/src/main/java/com/akrog/tolometgui2/ui/fragments/ChartsFragment.java
@@ -1,6 +1,7 @@
 package com.akrog.tolometgui2.ui.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.akrog.tolometgui2.R;
+import com.akrog.tolometgui2.model.AppSettings;
 import com.akrog.tolometgui2.ui.viewmodels.ChartsViewModel;
+import com.akrog.tolometgui2.ui.viewmodels.MainViewModel;
 
 public class ChartsFragment extends Fragment {
-
+    private AppSettings settings;
+    private MainViewModel model;
     private ChartsViewModel chartsModel;
+
+    private final Handler handler = new Handler();
+    private Runnable timer;
 
     public static ChartsFragment newInstance() {
         return new ChartsFragment();
@@ -30,8 +37,46 @@ public class ChartsFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        settings = AppSettings.getInstance();
+        model = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
         chartsModel = ViewModelProviders.of(this).get(ChartsViewModel.class);
-        // TODO: Use the ViewModel
+        createTimer();
     }
 
+    private void createTimer() {
+        cancelTimer();
+        if( settings.getUpdateMode() != AppSettings.AUTO_UPDATES )
+            return;
+        timer = new Runnable() {
+            @Override
+            public void run() {
+                if( model.checkStation() )
+                    downloadData();
+            }
+        };
+        //timer.run();
+    }
+
+    private void downloadData() {
+    }
+
+    private void cancelTimer() {
+        if( timer != null ) {
+            handler.removeCallbacks(timer);
+            timer = null;
+        }
+    }
+
+    private boolean postTimer() {
+        if( timer == null || settings.getUpdateMode() != AppSettings.AUTO_UPDATES )
+            return false;
+        handler.removeCallbacks(timer);
+        int minutes = 1;
+        if( model.checkStation() && !model.getCurrentStation().isEmpty() ) {
+            int dif = (int)((System.currentTimeMillis()-model.getCurrentStation().getStamp())/60/1000L);
+            minutes = dif >= model.getRefresh() ? 1 : model.getRefresh()-dif;
+        }
+        handler.postDelayed(timer, minutes*60*1000);
+        return true;
+    }
 }
