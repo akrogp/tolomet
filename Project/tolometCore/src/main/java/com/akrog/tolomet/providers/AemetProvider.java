cf8242f96ea338ad19bf45230757c544888df51e
Gorka Prieto <gorka.prieto@gmail.com>
2019-09-17 20:48:00 +0200

    New AEMET provider!

--- Project/tolometCore/src/main/java/com/akrog/tolomet/providers/AemetProvider.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/providers/AemetProvider.java
@@ -1,6 +1,5 @@
 package com.akrog.tolomet.providers;
 
-import com.akrog.tolomet.Header;
 import com.akrog.tolomet.Station;
 import com.akrog.tolomet.Utils;
 import com.akrog.tolomet.io.Downloader;
@@ -10,13 +9,18 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
 
 public class AemetProvider implements WindProvider {
-	
+
+	public AemetProvider() {
+		sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+	}
+
 	@Override
 	public String getInfoUrl(String code) {
 		return "http://www.aemet.es/es/eltiempo/observacion/ultimosdatos?l="+code+"&datos=det&w=0";
@@ -28,15 +32,12 @@ public class AemetProvider implements WindProvider {
 	}
 
 	private String download(String url) {
-		if( key == null )
-			try(BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/keys/aemet.txt")))) {
-				key = br.readLine();
-			} catch( Exception e) {
-				e.printStackTrace();
-				return null;
-			}
 		try {
-			Downloader dw = new Downloader();
+			if( key == null )
+				try(BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/keys/aemet.txt")))) {
+					key = br.readLine();
+				}
+			dw = new Downloader();
 			dw.setUrl(url);
 			dw.setHeader("api_key", key);
 			String data = dw.download();
@@ -48,6 +49,8 @@ public class AemetProvider implements WindProvider {
 			return data;
 		} catch( Exception e ) {
 			e.printStackTrace();
+		} finally {
+			dw = null;
 		}
 		return null;
 	}
@@ -77,13 +80,37 @@ public class AemetProvider implements WindProvider {
 
 	@Override
 	public void refresh(Station station) {
-		downloader = new Downloader();
-		downloader.setUrl("http://www.aemet.es/es/eltiempo/observacion/ultimosdatos.csv");
-		downloader.addParam("l",station.getCode());
-		downloader.addParam("datos","det");
-		downloader.addParam("x","h24");
-		downloader.addParam("w","0");
-		updateStation(station,downloader.download());
+		try {
+			String data = download("https://opendata.aemet.es/opendata/api/observacion/convencional/datos/estacion/" + station.getCode());
+			JSONArray array = new JSONArray(data);
+			for( int i = 0; i < array.length(); i++ ) {
+				JSONObject json = array.getJSONObject(i);
+				long date = sdf.parse(json.getString("fint")).getTime();
+				Number num;
+
+				num = json.optDouble("dv");
+				if( num != null )
+					station.getMeteo().getWindDirection().put(date, num);
+				num = json.optDouble("vv");
+				if( num != null )
+					station.getMeteo().getWindSpeedMed().put(date, num.floatValue()*3.6F);
+				num = json.optDouble("vmax");
+				if( num != null )
+					station.getMeteo().getWindSpeedMax().put(date, num.floatValue()*3.6F);
+
+				num = json.optDouble("ta");
+				if( num != null )
+					station.getMeteo().getAirTemperature().put(date, num);
+				num = json.optDouble("hr");
+				if( num != null )
+					station.getMeteo().getAirHumidity().put(date, num);
+				num = json.optDouble("pres");
+				if( num != null )
+					station.getMeteo().getAirPressure().put(date, num);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -93,107 +120,16 @@ public class AemetProvider implements WindProvider {
 
 	@Override
 	public void cancel() {
-		if( downloader != null )
-			downloader.cancel();	
+		if( dw != null )
+			dw.cancel();
 	}
 
 	@Override
 	public int getRefresh(String code) {
 		return 60;
 	}
-		
-	private void updateStation(Station station, String data) {
-		if( data == null )
-			return;
-		
-		String[] lines = data.replaceAll("\"","").split("\\n");
-		if( lines.length < 5 )
-			return;
-		Header index = getHeaders(lines[3]);
-		
-		long date; 
-		Number num, last = 0;
-		String cell;
-		station.clear();
-		for( int i = lines.length-1; i >= 4; i-- ) {
-			String[] cells = lines[i].split(",");
-			if( cells.length <= index.getDate() || (cell=cells[index.getDate()]).isEmpty() )
-				continue;
-			date = toEpoch(station.getRegion() == 178, cells[index.getDate()]);
-			if( index.getDir() < cells.length && !(cell=cells[index.getDir()]).isEmpty() ) {
-				num = parseDir(cells[index.getDir()]);
-				if( num == null )
-					num = last;
-				last = num;
-				station.getMeteo().getWindDirection().put(date, num);
-			}
-			if( index.getMed() < cells.length && !(cell=cells[index.getMed()]).isEmpty() ) {
-				num = Float.parseFloat(cell);
-				station.getMeteo().getWindSpeedMed().put(date, num);
-			}
-			if( index.getMax() < cells.length && !(cell=cells[index.getMax()]).isEmpty() ) {
-				num = Float.parseFloat(cell);
-				station.getMeteo().getWindSpeedMax().put(date, num);
-			}
-			if( index.getHum() > 0 && index.getHum() < cells.length && !(cell=cells[index.getHum()]).isEmpty() ) {
-				num = (float)(int)(Float.parseFloat(cell)+0.5F);
-				station.getMeteo().getAirHumidity().put(date, num);
-			}
-			if( index.getTemp() > 0 && index.getTemp() < cells.length && !(cell=cells[index.getTemp()]).isEmpty() ) {
-				num = Float.parseFloat(cell);
-				station.getMeteo().getAirTemperature().put(date, num);
-			}
-			if( index.getPres() > 0 && index.getPres() < cells.length && !(cell=cells[index.getPres()]).isEmpty() ) {
-				num = Float.parseFloat(cell);
-				station.getMeteo().getAirPressure().put(date, num);
-			}
-		}
-	}
-	
-	private Header getHeaders(String string) {
-		String[] cells = string.split(",");
-		Header index = new Header();
-		index.findDate("Fecha", cells);	// 0
-		index.findDir("Direc", cells);	// 3
-		index.findMed("Velo", cells);	// 2
-		index.findMax("Racha", cells);	// 4
-		index.findTemp("Temp", cells);	// 1
-		index.findHum("Hum", cells);	// 9
-		index.findPres("Pres", cells);	// 7
-		return index;
-	}
 
-	private Number parseDir( String dir ) {
-		if( dir.equalsIgnoreCase("Norte") )									
-			return 0;
-		else if( dir.equalsIgnoreCase("Noreste") )
-			return 45;
-		else if( dir.equalsIgnoreCase("Este") )
-			return 90;
-		else if( dir.equalsIgnoreCase("Sudeste") )
-			return 135;
-		else if( dir.equalsIgnoreCase("Sur") )
-			return 180;
-		else if( dir.equalsIgnoreCase("Sudoeste") )
-			return 225;
-		else if( dir.equalsIgnoreCase("Oeste") )
-			return 270;
-		else if( dir.equalsIgnoreCase("Noroeste") )
-			return 315;
-		return null; // Calma
-	}
-
-	private long toEpoch( boolean canary, String str ) {
-		Calendar cal = canary ? Calendar.getInstance(TimeZone.getTimeZone("Atlantic/Canary")) : Calendar.getInstance(TimeZone.getTimeZone("Europe/Madrid"));		
-		cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(str.substring(0,2)));
-		cal.set(Calendar.MONTH, Integer.parseInt(str.substring(3,5))-1);
-		cal.set(Calendar.YEAR, Integer.parseInt(str.substring(6,10)));
-		cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(str.substring(11,13)));
-		cal.set(Calendar.MINUTE, Integer.parseInt(str.substring(14)));
-		cal.set(Calendar.SECOND, 0);
-	    return cal.getTimeInMillis();
-	}	
-	
-	private Downloader downloader;
+	private Downloader dw;
 	private String key;
+	private final SimpleDateFormat sdf;
 }
