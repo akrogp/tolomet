f760d01cab2f203b2c14f482a62a25ddb111f8ec
Gorka Prieto <gorka.prieto@gmail.com>
2019-09-19 02:07:48 +0200

    Included support for downloading meteoclimatic stations

--- Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoClimaticProvider.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoClimaticProvider.java
@@ -4,8 +4,14 @@ import com.akrog.tolomet.Meteo;
 import com.akrog.tolomet.Station;
 import com.akrog.tolomet.io.Downloader;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gorka on 28/11/16.
@@ -24,6 +30,67 @@ public class MeteoClimaticProvider extends BaseProvider {
     @Override
     public boolean configureDownload(Downloader downloader, Station station, long date) {
         return false;
+    }
+
+    @Override
+    public List<Station> downloadStations() {
+        try {
+            List<Station> result = new ArrayList<>();
+            result.addAll(downloadStations("ES"));
+            result.addAll(downloadStations("PT"));
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            downloader = null;
+        }
+        return null;
+    }
+
+    private List<Station> downloadStations(String country) throws Exception {
+        downloader = new Downloader();
+        downloader.setUrl("https://www.meteoclimatic.com/feed/rss/" + country);
+        String data = downloader.download();
+        List<Station> result = new ArrayList<>();
+
+        XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(new StringReader(data));
+
+        int event = parser.getEventType();
+        String text = null;
+        String name = null, code = null;
+        Double lat = null, lon = null;
+        while( event != XmlPullParser.END_DOCUMENT ) {
+            String tag = parser.getName();
+            switch (event) {
+                case XmlPullParser.TEXT:
+                    text = parser.getText();
+                    break;
+                case XmlPullParser.END_TAG:
+                    if( tag.equals("title") )
+                        name = text.replaceAll(" \\(.*", "");
+                    else if( tag.equals("link") )
+                        code = text.replaceAll(".*/","");
+                    else if( tag.equals("geo:lat") )
+                        lat = Double.parseDouble(text);
+                    else if( tag.equals("geo:long") )
+                        lon = Double.parseDouble(text);
+                    else if( tag.equals("item") ) {
+                        Station station = new Station();
+                        station.setCode(code);
+                        station.setName(name);
+                        station.setLatitude(lat);
+                        station.setLongitude(lon);
+                        station.setProviderType(WindProviderType.MeteoClimatic);
+                        if( station.isFilled() )
+                            result.add(station);
+                    }
+                    break;
+            }
+            event = parser.next();
+        }
+        return result;
     }
 
     @Override
