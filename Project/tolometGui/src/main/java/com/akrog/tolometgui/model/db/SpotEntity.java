7e7bc838bda63cb2789ba4d9adc3def64045cc31
Gorka Prieto <gorka.prieto@gmail.com>
2019-09-25 18:52:03 +0200

    Included support for spots in DbTolomet

---
+++ Project/tolometGui/src/main/java/com/akrog/tolometgui/model/db/SpotEntity.java
@@ -0,0 +1,76 @@
+package com.akrog.tolometgui.model.db;
+
+import java.util.Date;
+
+public class SpotEntity {
+    private String id;
+    private double latitude, longitude;
+    private String name, desc;
+    private SpotType type;
+    private Date updated;
+    private SpotProvider provider;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public SpotType getType() {
+        return type;
+    }
+
+    public void setType(SpotType type) {
+        this.type = type;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public SpotProvider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(SpotProvider provider) {
+        this.provider = provider;
+    }
+}
