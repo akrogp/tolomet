906b686d0430f21cd4938cc661e61115ddbec827
Gorka Prieto <gorka.prieto@gmail.com>
2019-11-08 22:18:41 +0100

    Updated MeteoGalicia to the new API (for the moment the last 4h for performance reasons ...)

--- Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoGaliciaProvider.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/providers/MeteoGaliciaProvider.java
@@ -1,41 +1,119 @@
 package com.akrog.tolomet.providers;
 
+import com.akrog.tolomet.Meteo;
 import com.akrog.tolomet.Station;
 import com.akrog.tolomet.io.Downloader;
-import com.akrog.tolomet.io.XmlElement;
-import com.akrog.tolomet.io.XmlParser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.TimeZone;
 
 public class MeteoGaliciaProvider implements WindProvider {
-	public MeteoGaliciaProvider() {
-		loadParams();
+
+	@Override
+	public String getInfoUrl(String code) {
+		return "https://www.meteogalicia.gal/observacion/estacions/estacions.action?idEst="+code;
+	}
+
+	@Override
+	public String getUserUrl(String code) {
+		return String.format("https://www.meteogalicia.gal/observacion/meteovisor/indexChartDezHoxe.action?idEstacion=%s&nome=%s&dataSeleccionada=%s",
+			code, code, SDF_USER.format(new Date()));
+	}
+
+	@Override
+	public List<Station> downloadStations() {
+		try {
+			dw = new Downloader();
+			dw.setUrl("https://servizos.meteogalicia.gal/rss/observacion/listaEstacionsMeteo.action");
+			String data = dw.download();
+			JSONObject obj = new JSONObject(data);
+			JSONArray stations = obj.getJSONArray("listaEstacionsMeteo");
+			List<Station> result = new ArrayList<>(stations.length());
+			for( int i = 0; i < stations.length(); i++ ) {
+				JSONObject json = stations.getJSONObject(i);
+				Station station = new Station();
+				station.setCode(json.getInt("idEstacion")+"");
+				station.setName(json.getString("estacion"));
+				station.setLatitude(json.getDouble("lat"));
+				station.setLongitude(json.getDouble("lon"));
+				station.setProviderType(WindProviderType.MeteoGalicia);
+				result.add(station);
+			}
+			return result;
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			dw = null;
+		}
 	}
 		
 	@Override
 	public void refresh(Station station) {
-		String[] fields = urlParams.get(station.getCode()).split(":");
-		Calendar now = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		String time = String.format("%d/%d/%d", now.get(Calendar.DAY_OF_MONTH), now.get(Calendar.MONTH)+1, now.get(Calendar.YEAR) );
-		
-		downloader = new Downloader();
-		downloader.setUrl("http://www2.meteogalicia.es/galego/observacion/estacions/contidos/DatosHistoricosXML_dezminutal.asp");
-		downloader.addParam("est", station.getCode());
-		downloader.addParam("param", fields[1]);
-		downloader.addParam("data1", time);
-		downloader.addParam("data2", time);
-		downloader.addParam("idprov", fields[2]);
-		downloader.addParam("red", fields[3]);
-		updateStation(station, downloader.download());
+		Long stamp = station.getStamp();
+		Calendar prev = null;
+		if( stamp != null ) {
+			prev = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+			prev.setTimeInMillis(stamp);
+		}
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		cal.add(Calendar.HOUR_OF_DAY, 1);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		int stop = 4;
+		try {
+			while (stop-- >= 0) {
+				String dataFin = SDF_REST.format(cal.getTime());
+				cal.add(Calendar.HOUR_OF_DAY, -1);
+				String dataIni = SDF_REST.format(cal.getTime());
+				download(station, dataIni, dataFin);
+				if( prev != null && cal.before(prev) )
+					break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void download(Station station, String dataIni, String dataFin) throws Exception {
+		dw = new Downloader();
+		dw.setUrl("https://servizos.meteogalicia.es/rss/observacion/datos10min.action");
+		dw.addParam("cod", "ToloMet6");
+		dw.addParam("idEstacion", station.getCode());
+		dw.addParam("dataIni", dataIni);
+		dw.addParam("dataFin", dataFin);
+		String data = dw.download();
+		JSONObject obj = new JSONObject(data);
+		JSONArray array = obj.getJSONArray("list10Min");
+		Meteo meteo = station.getMeteo();
+		for( int i = 0; i < array.length(); i++ ) {
+			JSONObject json = array.getJSONObject(i);
+			long stamp = SDF_JSON.parse(json.getString("instanteLecturaUTC")).getTime();
+			JSONArray meas = json.getJSONArray("listaEstacions").getJSONObject(0).getJSONArray("listaMedidas");
+			for( int j = 0; j < meas.length(); j++ ) {
+				JSONObject m = meas.getJSONObject(j);
+				String param = m.getString("nomeParametro");
+				double value = m.getDouble("valor");
+				if( param.startsWith("Dirección do vento") )
+					meteo.getWindDirection().put(stamp, value);
+				else if( param.startsWith("Humidade") )
+					meteo.getAirHumidity().put(stamp, value);
+				else if( param.startsWith("Temperatura") )
+					meteo.getAirTemperature().put(stamp, value);
+				else if( param.startsWith("Velocidade") )
+					meteo.getWindSpeedMed().put(stamp, value*3.6);
+				else if( param.startsWith("Refacho") )
+					meteo.getWindSpeedMax().put(stamp, value*3.6);
+			}
+		}
 	}
 
 	@Override
@@ -45,132 +123,21 @@ public class MeteoGaliciaProvider implements WindProvider {
 
 	@Override
 	public void cancel() {
-		if( downloader != null )
-			downloader.cancel();	
-	}
-
-	protected void updateStation(Station station, String data) {
-		if( data == null )
-			return;
-		XmlElement root;
-		try {
-			root = XmlParser.load(new StringReader(data));
-		} catch (IOException e) {
-			return;
-		}
-		if( root == null || !root.getName().equals("Estacion") )
-			return;
-		
-		long date=0;
-		Number val;
-		station.clear();
-		for( XmlElement element : root.getSubElements() ) {
-			String name = element.getName();
-			if( !name.equals("Valores") )
-				continue;
-			date = toEpoch(element.getAttribute("Data"));
-			for( XmlElement meas : element.getSubElements() ) {					
-				//String attr = meas.getAttribute("ID");
-				String attr = meas.getAttribute("Variable").toLowerCase();
-				//if( attr.equals("82") || attr.equals("10124") ) {
-                if( attr.startsWith("direcc") ) {
-					val = Integer.parseInt(getContent(meas));
-					station.getMeteo().getWindDirection().put(date, val);
-				//} else if( attr.equals("81") ) {
-                } else if( attr.startsWith("veloci") ) {
-					station.getMeteo().getWindSpeedMed().put(date, getSpeed(meas));
-				//} else if( attr.equals("10003") ) {
-                } else if( attr.startsWith("refacho") ) {
-					station.getMeteo().getWindSpeedMax().put(date, getSpeed(meas));
-				//} else if( attr.equals("86") ) {
-                } else if( attr.startsWith("humi") ) {
-					try {
-						val = Float.parseFloat(getContent(meas));
-						station.getMeteo().getAirHumidity().put(date, val);
-					} catch( Exception e ) {}
-				//} else if( attr.equals("83") ) {
-                } else if( attr.startsWith("temp") ) {
-					try {
-						val = Float.parseFloat(getContent(meas));
-						station.getMeteo().getAirTemperature().put(date, val);
-					} catch( Exception e ) {}
-				//} else if( attr.equals("10002") ) {
-                } else if( attr.startsWith("pres") ) {
-					try {
-						val = Float.parseFloat(getContent(meas));
-						station.getMeteo().getAirPressure().put(date, val);
-					} catch( Exception e ) {}
-				}
-			}
-		}
-	}
-	
-	private float getSpeed( XmlElement meas ) {
-		float val = Float.parseFloat(getContent(meas));
-		if( meas.getAttribute("Unidades").equals("m/s") )
-			return val*3.6F;
-		return val;
+		if( dw != null )
+			dw.cancel();
 	}
 
 	@Override
 	public int getRefresh(String code) {
 		return 10;
-	}		
-	
-	private long toEpoch( String str ) {
-		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		String[] tmp = str.split(" ");
-		String[] date = tmp[0].split("/");
-		String[] time = tmp[1].split(":");
-		cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(date[0]));
-		cal.set(Calendar.MONTH, Integer.parseInt(date[1])-1);
-		cal.set(Calendar.YEAR, Integer.parseInt(date[2]));
-		cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]));
-		cal.set(Calendar.MINUTE, Integer.parseInt(time[1]));
-		cal.set(Calendar.SECOND, Integer.parseInt(time[2]));
-		cal.set(Calendar.MILLISECOND, 0 );
-	    return cal.getTimeInMillis();
-	}
-	
-	private String getContent( XmlElement element ) {
-		String val = element.getAttribute("Valor"); 	
-		return val.replace(',', this.separator);
-	}
-	
-	private void loadParams() {
-		urlParams = new HashMap<>();
-		
-		InputStream inputStream = getClass().getResourceAsStream("/data/meteogalicia.csv");
-		InputStreamReader in = new InputStreamReader(inputStream);
-		BufferedReader rd = new BufferedReader(in);
-		String line;
-		String[] fields;
-		try {
-			while( (line=rd.readLine()) != null ) {
-				fields = line.split(":");
-				urlParams.put(fields[0], line);
-			}
-			rd.close();
-		} catch( Exception e ) {			
-		}
-    }
-	
-	@Override
-	public String getInfoUrl(String code) {
-		return "http://www2.meteogalicia.es/galego/observacion/estacions/estacionsinfo.asp?Nest="+code;
 	}
 
-	@Override
-	public String getUserUrl(String code) {
-		return "http://www2.meteogalicia.es/galego/observacion/estacions/estacions.asp?idEst="+code;
+	private static SimpleDateFormat SDF_USER = new SimpleDateFormat("dd/MM/yyyy");
+	private static SimpleDateFormat SDF_REST = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	private static SimpleDateFormat SDF_JSON = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+	static {
+		SDF_REST.setTimeZone(TimeZone.getTimeZone("UTC"));
+		SDF_JSON.setTimeZone(TimeZone.getTimeZone("UTC"));
 	}
-
-	@Override
-	public List<Station> downloadStations() {
-		return null;
-	}
-
-	private Map<String,String> urlParams;
-	private final char separator = '.';
-	private Downloader downloader;	
+	private Downloader dw;
 }
