776a6aa07ff33e3767b0a3d6fe0dd985222d74ce
Gorka Prieto <gorka.prieto@gmail.com>
2020-03-22 18:17:34 +0100

    Updated WeatherDisplay stations

--- Project/tolometCore/src/main/java/com/akrog/tolomet/providers/WeatherDisplayProvider.java
+++ Project/tolometCore/src/main/java/com/akrog/tolomet/providers/WeatherDisplayProvider.java
@@ -5,6 +5,8 @@ import com.akrog.tolomet.Station;
 import com.akrog.tolomet.io.Downloader;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 /**
@@ -12,9 +14,35 @@ import java.util.TimeZone;
  */
 
 public class WeatherDisplayProvider extends BaseProvider {
-    private static final int REFRESH = 1;
+    private static final int REFRESH = 60;
     private static final TimeZone TIME_ZONE = TimeZone.getTimeZone("CET");
     private static final float KMH_KNOT = 1.852F;
+    private static Map<String, String> mapIndex;
+    private static Map<String, String> mapTxt;
+    private static int[] ISTAMP = {459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 578, 579, 580, 581};
+    private static int[] ITEMP = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 566, 567, 568, 569};
+    private static int[] IPRES = {439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 574, 575, 576, 577};
+    private static int[] IHUM = {611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634};
+    private static int[] IDIR = {536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 590, 591, 592, 593};
+    private static int[] ISPEED = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 562, 563, 564, 565};
+
+    private static Map<String, String> getMapIndex() {
+        if( mapIndex == null ) {
+            mapIndex = new HashMap<>();
+            mapIndex.put("FIOCHI", "https://tiempo.fiochi.com/wd_data/index.php");
+            mapIndex.put("RCNL", "https://www.rcnlaredo.es/~meteorcnl/meteo/wdisplay2/index.html");
+        }
+        return mapIndex;
+    }
+
+    private static Map<String, String> getMapTxt() {
+        if( mapTxt == null ) {
+            mapTxt = new HashMap<>();
+            mapTxt.put("FIOCHI", "https://tiempo.fiochi.com/clientrawextra.txt");
+            mapTxt.put("RCNL", "https://www.rcnlaredo.es/~meteorcnl/meteo/wdisplay2/clientrawextra.txt");
+        }
+        return mapTxt;
+    }
 
     public WeatherDisplayProvider() {
         super(REFRESH);
@@ -27,12 +55,12 @@ public class WeatherDisplayProvider extends BaseProvider {
 
     @Override
     public String getUserUrl(String code) {
-        return "http://tiempo.fiochi.com/wd_data/index.php";
+        return getMapIndex().get(code);
     }
 
     @Override
     public void configureDownload(Downloader downloader, Station station) {
-        downloader.setUrl("http://tiempo.fiochi.com/clientrawhour.txt");
+        downloader.setUrl(getMapTxt().get(station.getCode()));
     }
 
     @Override
@@ -42,39 +70,33 @@ public class WeatherDisplayProvider extends BaseProvider {
 
     @Override
     public void updateStation(Station station, String data) throws Exception {
-        Calendar calendar = getCalendar();
-        if( calendar == null )
-            return;
         String[] fields = data.split(" ");
-        if( fields.length < 674 )
+        if( fields.length < 635 )
             return;
-        Meteo meteo = station.getMeteo();
-        long stamp = calendar.getTimeInMillis()-60*60*1000;
-        for( int i = 0; i < 60; i++, stamp += 60*1000 ) {
-            meteo.getWindDirection().put(stamp, Float.parseFloat(fields[121+i]));
-            meteo.getWindSpeedMed().put(stamp, Float.parseFloat(fields[1+i])*KMH_KNOT);
-            meteo.getWindSpeedMax().put(stamp, Float.parseFloat(fields[61+i])*KMH_KNOT);
-            meteo.getAirTemperature().put(stamp, Float.parseFloat(fields[181+i]));
-            meteo.getAirHumidity().put(stamp, Float.parseFloat(fields[241+i]));
-            meteo.getAirPressure().put(stamp, Float.parseFloat(fields[301+i]));
-        }
-    }
 
-    private Calendar getCalendar() throws Exception {
-        Downloader downloader = new Downloader();
-        downloader.setUrl("http://tiempo.fiochi.com/clientraw.txt");
-        String info = downloader.download();
-        String[] fields = info.split(" ");
-        if( fields.length < 175 )
-            return null;
         Calendar cal = Calendar.getInstance(TIME_ZONE);
-        cal.set(Calendar.YEAR, Integer.parseInt(fields[141]));
-        cal.set(Calendar.MONTH, Integer.parseInt(fields[36])-1);
-        cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(fields[35]));
-        cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(fields[29]));
-        cal.set(Calendar.MINUTE, Integer.parseInt(fields[30]));
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
-        return cal;
+        int currentHour = cal.get(Calendar.HOUR_OF_DAY);
+        int currentMin = cal.get(Calendar.MINUTE);
+
+        Meteo meteo = station.getMeteo();
+        for( int i = 0; i < 24; i++ ) try {
+            String[] time = fields[ISTAMP[i]].split(":");
+            int prevHour = Integer.parseInt(time[0]);
+            int prevMin = Integer.parseInt(time[1]);
+            if( prevHour > currentHour || (prevHour == currentHour && prevMin > currentMin) )
+                continue;
+            cal.set(Calendar.HOUR_OF_DAY, prevHour);
+            cal.set(Calendar.MINUTE, prevMin);
+            long stamp = cal.getTimeInMillis();
+            meteo.getWindDirection().put(stamp, Float.parseFloat(fields[IDIR[i]]));
+            meteo.getWindSpeedMed().put(stamp, Float.parseFloat(fields[ISPEED[i]])*KMH_KNOT);
+            meteo.getAirTemperature().put(stamp, Float.parseFloat(fields[ITEMP[i]]));
+            meteo.getAirHumidity().put(stamp, Float.parseFloat(fields[IHUM[i]]));
+            meteo.getAirPressure().put(stamp, Float.parseFloat(fields[IPRES[i]]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
