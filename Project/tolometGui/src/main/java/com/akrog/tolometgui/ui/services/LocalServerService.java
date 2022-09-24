4487aebbea1141dc03fa9fae257d7dd2a95b1023
Gorka Prieto <gorka.prieto@gmail.com>
2022-09-24 19:55:21 +0200

    First version of local server (TO-TEST)

---
+++ Project/tolometGui/src/main/java/com/akrog/tolometgui/ui/services/LocalServerService.java
@@ -0,0 +1,184 @@
+package com.akrog.tolometgui.ui.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import com.akrog.tolomet.Manager;
+import com.akrog.tolomet.Station;
+import com.akrog.tolomet.utils.StringUtils;
+import com.akrog.tolometgui.R;
+import com.akrog.tolometgui.model.AppSettings;
+import com.akrog.tolometgui.model.db.DbMeteo;
+import com.akrog.tolometgui.model.db.DbTolomet;
+import com.akrog.tolometgui.model.db.MeteoDao;
+import com.akrog.tolometgui.model.db.StationDao;
+import com.akrog.tolometgui.ui.activities.MainActivity;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LocalServerService extends Service {
+
+    public LocalServerService() {
+        manager = new Manager(AppSettings.getInstance().getSelectedLanguage());
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        executor.execute(() -> {
+            try {
+                serve();
+            } catch (IOException e) {
+                e.printStackTrace();
+                stopSelf();
+            }
+        });
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if( !settings.isLocalServer() || (intent != null && ACTION_STOP.equals(intent.getAction())) ) {
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        createNotificationChannel();
+
+        int pendingIntentFlags = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? PendingIntent.FLAG_IMMUTABLE : 0;
+
+        Intent contentIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentPendingIntent =
+                PendingIntent.getActivity(this, 0, contentIntent, pendingIntentFlags);
+
+        Intent landIntent = new Intent(ACTION_STOP);
+        PendingIntent stopPendingIntent = PendingIntent.getBroadcast(this, 0, landIntent, pendingIntentFlags);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_SERVER)
+                .setSmallIcon(R.drawable.ic_wind_unknown)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_serving))
+                .setTicker(getString(R.string.notification_serving))
+                .setContentIntent(contentPendingIntent)
+                .addAction(R.drawable.ic_stop, getString(R.string.stop), stopPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            return;
+        CharSequence name = getString(R.string.server_channel_name);
+        String description = getString(R.string.notification_serving);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_SERVER, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    @Override
+    public void onDestroy() {
+        stop = true;
+        executor.shutdownNow();
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void serve() throws IOException {
+        DatagramSocket socket = new DatagramSocket(settings.getPortLocalServer());
+        socket.setSoTimeout(TIMEOUT);
+        byte[] buf = new byte[BUFFER];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        while(!stop) {
+            try{
+                socket.receive(packet);
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                byte[] output = process(packet).getBytes();
+                if( output != null )
+                    socket.send(new DatagramPacket(output, output.length, address, port));
+            } catch (SocketTimeoutException e) {
+            }
+        }
+        socket.close();
+    }
+
+    private String process(DatagramPacket packet) {
+        if( packet.getLength() < SIGNATURE.length() )
+            return INVALID;
+        String input = new String(packet.getData(), 0, packet.getLength());
+        String[] fields = input.split(SEP);
+        if(fields.length < 2 || !SIGNATURE.equals(fields[0]))
+            return INVALID;
+        String cmd = fields[1];
+        try {
+            if (cmd.equals(CMD_NEAR))
+                return processNear(Double.parseDouble(fields[2]), Double.parseDouble(fields[3]), Double.parseDouble(fields[4]), Double.parseDouble(fields[5]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return INVALID;
+    }
+
+    private String processNear(double lat1, double lon1, double lat2, double lon2) {
+        StringBuilder output = new StringBuilder(CMD_NEAR);
+        for(Station station : db.findGeoStations(lat1, lon1, lat2, lon2)) {
+            output.append("\n");
+            station = meteo.loadStation(station).getValue();
+            manager.refresh(station);
+            long stamp = station.getStamp();
+            output.append(StringUtils.toCsv(SEP,
+                station.getId(),
+                station.getName(),
+                station.getProviderType().name(),
+                station.getProviderType().getQuality().name(),
+                StringUtils.formatDecimal(station.getLatitude()),
+                StringUtils.formatDecimal(station.getLongitude()),
+                stamp,
+                StringUtils.formatDecimal(station.getMeteo().getWindDirection().getAt(stamp)),
+                StringUtils.formatDecimal(station.getMeteo().getWindSpeedMed().getAt(stamp)),
+                StringUtils.formatDecimal(station.getMeteo().getWindSpeedMax().getAt(stamp))
+            ));
+        }
+        return output.toString();
+    }
+
+    //public static final String ACTION_START = "com.akrog.tolomet.action.server.start";
+    public static final String ACTION_STOP = "com.akrog.tolomet.action.server.stop";
+    public static final int TIMEOUT = 1000;
+    public static final int BUFFER = 30000;
+    public static final String SIGNATURE = "TOLO";
+    public static final String SEP = "\t";
+    public static final String INVALID = "Invalid request";
+    public static final String CMD_NEAR = "NEAR";
+    private static final String CHANNEL_SERVER = "com.akrog.tolomet.channel.server";
+    private static final int ONGOING_NOTIFICATION_ID = 3001;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private volatile boolean stop = false;
+    private final StationDao db = DbTolomet.getInstance().stationDao();
+    private final MeteoDao meteo = DbMeteo.getInstance().meteoDao();
+    private final Manager manager;
+    private final AppSettings settings = AppSettings.getInstance();
+}
