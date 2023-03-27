f1d7683cf671ad0b1419e180f5d16df7c750b178
Gorka Prieto <gorka.prieto@gmail.com>
2023-03-27 14:18:13 +0200

    Fixed widgets again

--- Project/tolometGui/src/main/java/com/akrog/tolometgui/widget/model/WidgetModel.java
+++ Project/tolometGui/src/main/java/com/akrog/tolometgui/widget/model/WidgetModel.java
@@ -2,6 +2,9 @@ package com.akrog.tolometgui.widget.model;
 
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
 import com.akrog.tolomet.Manager;
 import com.akrog.tolomet.Station;
 import com.akrog.tolometgui.model.db.DbMeteo;
@@ -15,7 +18,6 @@ public class WidgetModel {
     private final WidgetManager widgetManager;
     private final Manager stationManager;
     private final List<WidgetInfo> widgets;
-    private List<Station> stations;
 
     public WidgetModel(Context context) {
         widgetManager = new WidgetManager(context);
@@ -24,18 +26,34 @@ public class WidgetModel {
     }
 
     public void download() {
-        stations = new ArrayList<>(widgets.size());
-        for( WidgetInfo widget : widgets ) {
-            String stationId = widget.getFlySpot().getConstraints().get(0).getStation();
-            Station station = DbTolomet.getInstance().stationDao().findStation(stationId);
+        for( Station station : findStations() ) {
             stationManager.refresh(station);
             DbMeteo.getInstance().meteoDao().saveStation(station);
-            stations.add(station);
         }
     }
 
     public void update() {
-        for( int i = 0; i < widgets.size(); i++ )
-            widgetManager.updateWidget(widgets.get(i), stations.get(i));
+        List<Station> stations = findStations();
+        for( int i = 0; i < widgets.size(); i++ ) {
+            LiveData<Station> liveStation = DbMeteo.getInstance().meteoDao().loadStation(stations.get(i));
+            WidgetInfo widget = widgets.get(i);
+            liveStation.observeForever(new Observer<Station>() {
+                @Override
+                public void onChanged(Station station) {
+                    widgetManager.updateWidget(widget, station);
+                    liveStation.removeObserver(this);
+                }
+            });
+        }
+    }
+
+    private List<Station> findStations() {
+        List<Station> stations = new ArrayList<>(widgets.size());
+        for( WidgetInfo widget : widgets ) {
+            String stationId = widget.getFlySpot().getConstraints().get(0).getStation();
+            Station station = DbTolomet.getInstance().stationDao().findStation(stationId);
+            stations.add(station);
+        }
+        return stations;
     }
  }
