c19d4410a50306106b8de4aafcc9b77e1b099dfa
Gorka Prieto <gorka.prieto@gmail.com>
2023-04-19 19:48:36 +0200

    BUGFIX: chart fragment views are now correctly updated

--- Project/tolometGui/src/main/java/com/akrog/tolometgui/ui/presenters/MySummary.java
+++ Project/tolometGui/src/main/java/com/akrog/tolometgui/ui/presenters/MySummary.java
@@ -3,15 +3,16 @@ package com.akrog.tolometgui.ui.presenters;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.TextView;
+
+import androidx.lifecycle.ViewModelProvider;
 
 import com.akrog.tolometgui.R;
 import com.akrog.tolometgui.model.AppSettings;
 import com.akrog.tolometgui.ui.activities.ToolbarActivity;
 import com.akrog.tolometgui.ui.viewmodels.MainViewModel;
 import com.akrog.tolometgui.ui.views.Axis;
-
-import androidx.lifecycle.ViewModelProviders;
 
 public class MySummary implements Presenter, Axis.ChangeListener {
 	private ToolbarActivity activity;
@@ -21,12 +22,12 @@ public class MySummary implements Presenter, Axis.ChangeListener {
 	private Long stamp = null;
 	
 	@Override
-	public void initialize(ToolbarActivity activity, Bundle bundle) {
+	public void initialize(ToolbarActivity activity, View view) {
 		this.activity = activity;
-		model = ViewModelProviders.of(activity).get(MainViewModel.class);
+		model = new ViewModelProvider(activity).get(MainViewModel.class);
 		settings = AppSettings.getInstance();
 
-		summary = (TextView)activity.findViewById(R.id.textSummary);
+		summary = (TextView)view.findViewById(R.id.textSummary);
 	}
 	
 	@Override
