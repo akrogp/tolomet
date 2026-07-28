2695a49c0c027a49c92bb56c5efd20dd391b801c
Marco Quartulli <marco.quartulli@gmail.com>
2026-07-28 22:04:12 +0200

    Working iOS app

--- Project/iosAppSimple/TolometSimple/TolometSimpleApp.swift
+++ Project/iosAppSimple/TolometSimple/TolometSimpleApp.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct TolometApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                WindStationView()
+            }
         }
     }
 }
